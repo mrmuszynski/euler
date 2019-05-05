f674a54d7e5ffd8e8077a22a7f317df78b68c451
Matt Muszynski <matthew.muszynski@jpl.nasa.gov>
2019-05-05 16:00:46 -0700

    solves problem 27

--- euler27.py
+++ euler27.py
@@ -24,11 +24,61 @@
 # D-
 ###############################################################################
 
-import numpy as np
+from numpy import arange, set_printoptions, load, meshgrid, array
+from numpy import unique, logical_or, zeros
 import pdb
-np.set_printoptions(precision=64)
-for d in range(1,1001):
-	print((1/d))
+set_printoptions(precision=64)
+
+primes = load('primes.npy')
+a = arange(-1000,1001)
+b = arange(-1000,1001)
+
+aa, bb = meshgrid(a,b)
+aa = aa.reshape(2001*2001)
+bb = bb.reshape(2001*2001)
+aaa = aa
+bba = bb
+cc = aa*bb
+dd = arange(2001*2001)
+
+def poly(n,a,b):
+	return n**2 + aa*n + bb
+
+def filterNonPrimes(polyResult):
+	uniquePolyResults = unique(polyResult)
+	uniquePolyPrimes = [x for x in uniquePolyResults if x in primes]
+	logical_result = zeros(len(polyResult))
+	for x in uniquePolyPrimes:
+		logical_result = logical_or(logical_result, polyResult == x)
+	return logical_result
+
+n = 0
+polyResult = []
+while len(polyResult) != 1:
+	polyResult = poly(n,aa,bb)
+	print(len(polyResult))
+	index = filterNonPrimes(polyResult)
+	print(len(polyResult))
+	print (index)
+	aa = aa[index]
+	bb = bb[index]
+	dd = dd[index]
+	polyResult = polyResult[index]
+	print(dd[-1])
+	print(len(dd))
+	n +=1
+
+print(aaa[dd[0]]*bba[dd[0]])
+pdb.set_trace()
+
+
+
+
+
+
+
+
+
 
 
 
